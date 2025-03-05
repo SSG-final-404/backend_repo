3b12bd9d6358dd5c1ba99209fec52feed9fc0ecb
Sojung Lee <dlthwjd1118@gmail.com>
2025-03-05 09:45:58 +0900

    add : backend 초기설정

---
+++ src/main/java/com/exam/Pj404Application.java
@@ -0,0 +1,13 @@
+package com.exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pj404Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pj404Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/exam/Pj404ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pj404ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
